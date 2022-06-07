3ec758ce1214f0652aa0daae4c0566809a0fd8e7
Martin Karry <martin@karry.de>
2022-06-07 16:03:58 +0200

    Fixed missing definition in ContentTypeComplex

--- packages/common/src/model/content/ContentTypeComplex.interface.ts
+++ packages/common/src/model/content/ContentTypeComplex.interface.ts
@@ -1,5 +1,7 @@
 import { ContentTypeCommon } from "./ContentTypeCommon.interface";
+import { FieldType } from "./fieldTypes";
 
 export interface ContentTypeComplex<T> extends ContentTypeCommon<T> {
-    type: "COMPLEX"
+    type: "COMPLEX",
+    definition: {[fielName:string]: FieldType}
 }
