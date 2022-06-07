06d76e6aed5a04611f22a7764c40a0714998c2f8
Martin Karry <martin@karry.de>
2022-06-07 16:06:01 +0200

    Made condition in FieldTypeCommon optional

--- packages/common/src/model/content/fieldTypes/FieldTypeCommon.interface.ts
+++ packages/common/src/model/content/fieldTypes/FieldTypeCommon.interface.ts
@@ -3,5 +3,5 @@ import { DiscriminatorFieldType } from "./DiscriminatorFieldType.type";
 export interface FieldTypeCommon<T = any> {
     type: DiscriminatorFieldType,
     hidden?: boolean,
-    condition: (value:T)=> Promise<boolean>
+    condition?: (value:T)=> Promise<boolean>
 }
