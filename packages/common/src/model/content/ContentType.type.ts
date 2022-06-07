5e959a9185132fd4698df730961668ea59ffb4d5
Martin Karry <martin@karry.de>
2022-06-07 17:50:15 +0200

    Fixed generic

--- packages/common/src/model/content/ContentType.type.ts
+++ packages/common/src/model/content/ContentType.type.ts
@@ -1,4 +1,4 @@
 import { ContentTypeComplex } from "./ContentTypeComplex.interface";
 import { ContentTypeFile } from "./ContentTypeFile.interface";
 
-export type ContentType<T = any> = ContentTypeComplex<T> | ContentTypeFile<T>;
+export type ContentType<T = any> = ContentTypeComplex | ContentTypeFile;
