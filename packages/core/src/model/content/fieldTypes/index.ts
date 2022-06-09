cbaf3eb231a3733c1be9295753ac3ade9c2f0727
Martin Karry <martin@karry.de>
2022-06-09 16:48:16 +0200

    Added and tested isFieldType

--- packages/core/src/model/content/fieldTypes/index.ts
+++ packages/core/src/model/content/fieldTypes/index.ts
@@ -1,1 +1,7 @@
+export * from "./isFieldType.function"
 export * from "./validateFieldType.function"
+export * from "./validateFieldTypeBoolean.function"
+export * from "./validateFieldTypeComplex.function"
+export * from "./validateFieldTypeNumber.function"
+export * from "./validateFieldTypeString.function"
+export * from "./validateFieldsDefinition.function"
