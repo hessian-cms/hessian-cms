640ed34e613cf84a8eaff055d8b91fca078de191
Martin Karry <martin@karry.de>
2022-08-14 22:22:46 +0200

    Fixed linter issues

--- packages/api/src/hessian/Hessian.class.ts
+++ packages/api/src/hessian/Hessian.class.ts
@@ -1,23 +1,5 @@
-import { ContentType } from "@hessian-cms/common";
 import { PersistenceManager } from "../persistence";
-import { HessianContentTypeManager } from "./HessianContentTypeManager.class";
 
 export class Hessian {
     constructor(private config: PersistenceManager) { }
-
-    createContentType(name: string, contentType: ContentType):Promise<string> {
-        return new Promise<string>(()=>{})
-    }
-
-    getContentType(name:string): Promise<HessianContentTypeManager> {
-        return new Promise<HessianContentTypeManager>(()=>{})
-    }
-
-    removeContentType(name:string):Promise<string> {
-        return new Promise<string>(()=>{})
-    }
-
-    alterContentType(name:string, contentType:ContentType): Promise<string> {
-        return new Promise<string>(()=>{})
-    }
 }
--- packages/api/src/persistence/local/LocalPersistenceManager.class.ts
+++ packages/api/src/persistence/local/LocalPersistenceManager.class.ts
@@ -1,42 +1,30 @@
-import { ContentType, validateContentType } from "@hessian-cms/common";
-import { existsSync, readFileSync } from "fs";
+import { ContentType } from "@hessian-cms/common";
 import { PersistenceManager } from "../PersistenceManager.abstract.class";
 
-const CONTENT_TYPES_FILENAME: string = "hessian.types.json"
+const CONTENT_TYPES_FILENAME = "hessian.types.json"
 
 export class LocalPersistenceManager extends PersistenceManager {
     private path2ContentTypes: string = this.path + this.path.endsWith("/") ? "" : "/" + CONTENT_TYPES_FILENAME;
     private contentTypes: ContentType[] = [];
     constructor(private path: string) {
         super();
-
-        this.load = this.load.bind(this);
-        this.load();
     }
 
-    private load() {
-        if (existsSync(this.path2ContentTypes)) {
-            const contentTypesFromFile = JSON.parse(readFileSync(this.path2ContentTypes).toString());
-            if (contentTypesFromFile instanceof Array) {
-                
-            }
-        }
-    }
 
     registerContentType(slug: string, contentType: ContentType): Promise<ContentType> {
-        throw new Error("Method not implemented.");
+        throw new Error(`Method not implemented.${slug}${contentType}`);
     }
     getContentType(slug: string): Promise<ContentType> {
-        throw new Error("Method not implemented.");
+        throw new Error(`Method not implemented.${slug}`);
     }
     getAllContentTypes(): Promise<ContentType[]> {
         throw new Error("Method not implemented.");
     }
     updateContentType(slug: string, contentType: ContentType): Promise<ContentType> {
-        throw new Error("Method not implemented.");
+        throw new Error(`Method not implemented.${slug}${contentType}`);
     }
     removeContentType(slug: string): Promise<string> {
-        throw new Error("Method not implemented.");
+        throw new Error(`Method not implemented.${slug}`);
     }
 
 }
